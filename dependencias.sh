5349ea5a1b9833ac4c21e734ac09ff86a4adf825
Rafael Queiroz <66445978+raaqueiroz@users.noreply.github.com>
2022-05-02 21:20:48 -0300

    install git

--- dependencias.sh
+++ dependencias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt-get install ansible git -y
 
 git clone https://github.com/raaqueiroz/queiroz-vorx-counter-app.git /tmp/pointer-app/
 
