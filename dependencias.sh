669886ec026d1ea13961f3cc049430636d760242
Rafael Queiroz <66445978+raaqueiroz@users.noreply.github.com>
2022-05-02 21:08:44 -0300

    iac

--- dependencias.sh
+++ dependencias.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo apt-get update
+sudo apt-get install ansible -y
+
+git clone https://github.com/raaqueiroz/queiroz-vorx-counter-app.git /tmp/pointer-app/
+
+cd /tmp/pointer-app/
+sudo ansible-playbook pointer-playbook.yml
